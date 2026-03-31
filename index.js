a212c714dbe2c74611d8076d58ff7ae2e481d148
PYRO DRYAD 69 <84041415+SLYAER@users.noreply.github.com>
2026-03-31 23:43:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const config = require('./config.json');
+const { cleanupQueue } = require('./systems/matchmaking');
 
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.commands = new Collection();
+
+// Load commands
+fs.readdirSync('./commands').forEach(file => {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+});
+
+// MongoDB
+mongoose.connect(config.mongoURI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(console.error);
+
+// Events
+client.on('interactionCreate', require('./events/interactionCreate')(client));
+
+// Queue cleanup
+setInterval(() => {
+  cleanupQueue();
+}, 30000);
+
+client.login(config.token);
